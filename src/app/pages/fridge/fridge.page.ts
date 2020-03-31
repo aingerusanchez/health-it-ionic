ad5727eb0c7811af4bf0a9cc203bdf995e20c4a7
Aingeru Sanchez <aingerusanchez72@gmail.com>
2020-04-01 00:23:24 +0200

    feat(nevera): añadir/consumir alimentos en la lista

--- src/app/pages/fridge/fridge.page.ts
+++ src/app/pages/fridge/fridge.page.ts
@@ -7,6 +7,41 @@ import { Component } from '@angular/core';
 })
 export class FridgePage {
 
-  constructor() {}
+  public foodList = [
+    {
+      name: 'Pechugas',
+      amount: 3
+    },
+    {
+      name: 'Hamburguesas',
+      amount: 2
+    },
+    {
+      name: 'Pizza',
+      amount: 2
+    },
+    {
+      name: 'Lomo',
+      amount: 4
+    }
+  ];
+  public newFood = '';
+
+  constructor() { }
+
+  /** Add a new food to the `foodList` */
+  public addNewFood() {
+    if (this.newFood) {
+      console.debug('LOG: FridgePage.addNewFood -> newFood', this.newFood);
+      this.foodList.push({ name: this.newFood, amount: 1 });
+      this.newFood = '';
+    }
+  }
+
+  /** Delete a food from `foodList` based on food index */
+  public consume(foodIndex: number) {
+    console.debug('LOG: FridgePage -> consume -> foodIndex', foodIndex);
+    this.foodList.splice(foodIndex, 1);
+  }
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 export enum TabRef {
-  NEVERA = 'NEVERA'
+  NEVERA = 'fridge'
 }
 
 @Component({
