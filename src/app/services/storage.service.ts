3458a3d5fe91bd5ef936c818782940e638ff6bf9
Aingeru Sanchez <aingerusanchez72@gmail.com>
2020-04-01 22:31:22 +0200

    Servicio para gestión del Storage Local
    Utiliza el Plugin Storage de Capacitor; este selecciona el storage local más conveniente para cada plataforma

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+
+export enum StorageKey {
+  FOOD_LIST = 'foodList'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  async setObject(key: string, object: any) {
+    await Storage.set({
+      key,
+      value: JSON.stringify(object)
+    });
+  }
+
+  async getObject(key: string) {
+    const ret = await Storage.get({ key });
+    return JSON.parse(ret.value);
+  }
+
+  async setItem(key: string, value: string) {
+    await Storage.set({
+      key,
+      value
+    });
+  }
+
+  async getItem(key: string) {
+    return await Storage.get({ key });
+  }
+
+  async removeItem(key: string) {
+    await Storage.remove({ key });
+  }
+
+  async keys() {
+    return await Storage.keys();
+  }
+
+  async clear() {
+    await Storage.clear();
+  }
+}
