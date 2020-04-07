2af363b6da8136796f93e91acd4e3f38aef426f5
Aingeru Sanchez <aingerusanchez72@gmail.com>
2020-04-07 11:34:10 +0200

    chore(): ajustes para cargar la version de package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+declare var require: any;
 const version = require('package.json').version;
 
 export const environment = {
   production: true,
+  appName: 'Health-It!',
   version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+declare var require: any;
 const version = require('package.json').version;
 
 export const environment = {
   production: false,
+  appName: 'Health-It!',
   version
 };
 
